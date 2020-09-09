768c06b1f208c99c4e5ac80ee7edd8a38a0033be
Carltorix <carlosacomba@gmail.com>
2020-09-09 16:48:46 -0300

    template iniciar_sesion y crear cuenta modificaciones en perfil

--- proyecto_etapa2/cuenta/models.py
+++ proyecto_etapa2/cuenta/models.py
@@ -4,26 +4,28 @@ from django.contrib.auth.models import AbstractUser
 class Perfil(AbstractUser):
     nacimiento = models.DateField(null = True)
     foto = models.ImageField(upload_to = "foto_perfil", null = True, blank = True)
-    permitir_comentarar = models.BooleanField(default = True)
+    localidad = models.CharField(max_length = 60)
+    provincia = models.CharField(max_length = 30)
+    telefono = models.CharField(max_length = 20)
 
 class Perfil_trabajador(Perfil):
     experiencia_laboral = models.CharField(max_length = 200,null = True)
     recomendaciones = models.IntegerField()
-    
-class Titulo_trabajador(models.Model):
-    trabajor = models.ForeignKey(Perfil_trabajador, on_delete = models.CASCADE)
-    titulo = models.ForeignKey(Titulo, on_delete = models.CASCADE) 
+    titulo = models.ManyToManyField("Perfil", blank=True, related_name="mis_titulos")
     
 class Titulo(models.Model):
     titulo = models.CharField(max_length = 30)
     matricula = models.CharField(max_length = 30,null = True)
+    def __str__(self):
+        return self.titulo
+
 
 class Categoria(models.Model):
     nombre_cat = models.CharField(max_length = 30)
     descripcion = models.TextField()
-    titulo = models.ForeignKey(Titulo, on_delete = models.CASCADE) 
+    titulo = models.ManyToManyField("Perfil", blank=True, related_name="titulos_categoria")
     def __str__(self):
-        return self.titulo
+        return self.nombre_cat
 
 class Comentario(models.Model):
     com_trabajor = models.ForeignKey(Perfil_trabajador, on_delete = models.CASCADE)

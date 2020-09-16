9fe6b64bb2fa4626aab84c4182300098211b13f9
Carltorix <carlosacomba@gmail.com>
2020-09-16 12:33:11 -0300

    fusion 2

--- proyecto_etapa2/bolsa/models.py
+++ proyecto_etapa2/bolsa/models.py
@@ -6,11 +6,11 @@ from cuenta.models import Perfil_trabajador, Categoria
 class Oferta(models.Model):
     titulo = models.CharField(max_length=30)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to="post/", null=True)
+    imagen = models.ImageField(upload_to="oferta/", null=True)
     oferente = models.ForeignKey(Perfil_trabajador, on_delete = models.CASCADE,default=None, related_name="oferta_creados")
     fecha_creado = models.DateTimeField(auto_now_add=True)
     fecha_modificado = models.DateTimeField(auto_now=True)
     categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL,null=True)
-    fecha_caducacion =models.DateTimeField()
+    fecha_caducacion =models.DateTimeField(null=True)
     def __str__(self):
         return self.titulo
